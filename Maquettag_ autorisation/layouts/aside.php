7c36113e7e8b8e85ceda34769a0d0c8996d49bb0
Hamid Achaou <hamidAchaou379@gmail.com>
2024-01-04 12:03:55 +0100

    create layouts

---
+++ Maquettag_ autorisation/layouts/aside.php
@@ -0,0 +1,53 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4 position-fixed">
+  <!-- Logo de la marque -->
+  <a href="../../droit_d'accés/Autorisations/index.php" class="brand-link">
+    <img src="../../adminlte/dist/img/gestion-de-projet.png" class="brand-image img-circle elevation-3" alt="Image de groupe">
+    <span class="brand-text font-weight-light text-center">Gestion des Projets</span>
+  </a>
+
+  <!-- Barre latérale -->
+  <div class="sidebar">
+    <!-- Menu latéral -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-user-lock"></i>
+              <p>
+                Droit d'accès
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../../droit_d'accés/Autorisations/index.php" class="nav-link active">
+                  <i class="far fa-check-circle nav-icon"></i>
+                  <p>Autorisation</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../../droit_d'accés/Roles/index.php" class="nav-link">
+                  <i class="far fa-user-circle nav-icon"></i>
+                  <p>Rôle</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../../droit_d'accés/Controllers/index.php" class="nav-link ">
+                  <i class="fas fa-gamepad nav-icon"></i>
+                  <p>Controllers</p>
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a href="../../droit_d'accés/Actions/index.php" class="nav-link ">
+                  <i class="fas fa-cogs nav-icon"></i>
+                  <p>Actions</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
